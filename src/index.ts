8168f0b05ceba19876e1acce74fc4ada64f47e18
Richard Shephard <richards@sahomeloans.com>
2018-03-23 20:54:01 +0200

    Add bootstrap-vue (without module found??). Added test alerts.

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,12 @@ import Vue from 'vue';
 import router from 'router/router';
 import App from 'pages/app.vue';
 
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+Vue.use(BootstrapVue);
+
 new Vue({
   router,
   el: "#app",
