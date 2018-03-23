b89492196ab2195fc8cfd080f29c63eed7a752ca
Richard Shephard <richards@sahomeloans.com>
2018-03-23 22:45:39 +0200

    Move product form to component with emit to parent save

---
+++ src/components/products/productsForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <form @submit.prevent="saveProduct">
+      <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
+        <div class="form-group">
+          <label>Name</label>
+            <input type="text" placeholder="Name" v-model="model.name" v-validate="'required'" name="name" :class="{'form-control': true, 'error': errors.has('name')}" />
+            <span class="small text-danger" v-show="errors.has('name')">Name is required</span>
+        </div>
+        <div class="form-group new-button">
+          <button class="button">
+            <i class="fas fa-pencil-alt"></i>
+            <span v-if="isEditing">Update Product</span>
+            <span v-else>Add Product</span>
+          </button>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  props: ['model', 'isEditing'],
+  methods: {
+    saveProduct() {
+      this.$emit('save-product', this.model);
+    }
+  }
+}
+</script>
--- src/pages/admin/new.vue
+++ src/pages/admin/new.vue
@@ -1,28 +1,15 @@
 <template>
   <div>
-    <form @submit.prevent="saveProduct">
-      <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
-        <div class="form-group">
-          <label>Name</label>
-            <input type="text" placeholder="Name" v-model="model.name" v-validate="'required'" name="name" :class="{'form-control': true, 'error': errors.has('name')}" />
-            <span class="small text-danger" v-show="errors.has('name')">Name is required</span>
-        </div>
-        <div class="form-group new-button">
-          <button class="button">
-            <i class="fas fa-pencil-alt"></i>
-            <span v-if="isEditing">Update Product</span>
-            <span v-else>Add Product</span>
-          </button>
-        </div>
-      </div>
-    </form>
+    <product-form @save-product="addProduct" :model="model"></product-form>
   </div>
 </template>
 <script>
+import ProductForm from 'components/products/productsForm.vue';
+
 export default {
   methods: {
-    saveProduct() {
-      console.log(this.errors);
+    addProduct(model) {
+      console.log(model);
     }
   },
   data() {
@@ -30,6 +17,9 @@ export default {
       model: {},
       isEditing: false
     }
+  },
+  components: {
+    'product-form': ProductForm
   }
 }
 </script>
