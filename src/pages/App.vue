ceaea8b9dcd3c3512f1a2ac7d9bc6f56a9908dd0
Richard Shephard <richards@sahomeloans.com>
2018-05-09 21:33:35 +0200

    Updated nav style

--- src/pages/App.vue
+++ src/pages/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
     <b-navbar toggleable="md" type="dark" variant="dark">
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
       <b-collapse is-nav id="nav_collapse">
         <b-navbar-nav>
-          <b-nav-item><router-link to="/" class="nav-link">Home</router-link></b-nav-item>
-          <b-nav-item><router-link to="/admin" class="nav-link">Admin</router-link></b-nav-item>
-          <b-nav-item><router-link to="/cart" class="nav-link">Cart</router-link></b-nav-item>
+          <b-nav-item><router-link to="/" class="nav-link"><i class="nav-icon fas fa-home"></i>Home</router-link></b-nav-item>
+          <b-nav-item><router-link to="/admin" class="nav-link"><i class="nav-icon fas fa-user"></i>Admin</router-link></b-nav-item>
+        </b-navbar-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item><router-link to="/cart" class="nav-link"><i class="nav-icon fas fa-shopping-cart"></i>Cart</router-link></b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
@@ -19,3 +22,12 @@ export default {
 }
 </script>
 
+<style lang="scss" scoped>
+.nav-link {
+  margin-right: 5px;
+}
+
+.nav-icon {
+  margin-right: 10px;
+}
+</style>
